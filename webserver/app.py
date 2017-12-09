77e2ff7e2e864c719cb6b2067a55dd3f9c18bf45
Ken Takaki <kenkiki94305@gmail.com>
2017-12-10 01:49:11 +0900

    tuning

--- webserver/app.py
+++ webserver/app.py
@@ -6,12 +6,13 @@ app = Flask(__name__)
 arm_pubs = []
 table_pub = None
 furikake_pub = None
+STEP = 10
 import time
 place_matrix = [[325, 384, 280, 487, 348],
                 [325, 325, 280, 433, 348],
                 [249, 348, 249, 492, 451],
                 [249, 334, 280, 438, 433]]
-current_arm = [0, 0, 0, 0, 0, 0]
+current_arm = [325, 384, 280, 487, 348, 300]
 @app.route("/obento")
 def obento_make():
     bento_id = requests.args.get('id', default=0, type=int)
@@ -35,21 +36,33 @@ def open_hand():
     move_arm(5, 231)
 
 def move_arm_gradual(idx, value):
-    prev_value = current_arm[idx]
-    while current_arm[idx] != value:
-        if current_arm[idx] < value:
-            
-    time.sleep(0.01)
-    
+    value = int(value)
+    next_value = current_arm[idx]
+
+    if current_arm[idx] < value:
+        next_value = min(current_arm[idx] + STEP, value)
+    else:
+        next_value = max(current_arm[idx] - STEP, value)
+    move_arm(idx, next_value)
+    print(str(value) + "," + str(current_arm[idx]))
+    print(int(current_arm[idx]) == int(value))
+    return int(current_arm[idx]) == int(value)
+
+
 def move_arm(idx, value):
+    global arm_pubs
     arm_pubs[idx].publish(value)
     current_arm[idx] = value
             
 def go_to(idx):
     if idx < 0 or idx > len(place_matrix[0])-1:
         idx = 0
-    for i in range(5):
-        move_arm(i, place_matrix[idx][i])
+    finish_mat = [False] * 5
+    while not all(finish_mat):
+        for i in range(5):
+            finished = move_arm_gradual(i, place_matrix[idx][i])
+            finish_mat[i] = finished
+        time.sleep(0.1)
 
 def go_home():
     arm_pubs[0].publish(325)
